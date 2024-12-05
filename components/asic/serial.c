f45d58070249ee2d89060bc2f0249d3b5091d437
ACSBEN <ben@advancedcryptoservices.com>
2024-12-04 22:02:06 -0600

    added second serial function framework for BAP Port

--- components/asic/serial.c
+++ components/asic/serial.c
@@ -18,6 +18,10 @@
 #define ECHO_TEST_RXD (18)
 #define BUF_SIZE (1024)
 
+#define UARTBAP_TXD (40)
+#define UARTBAP_RXD (41)
+#define BAP_BUF_SIZE (1024)
+
 static const char *TAG = "serial";
 
 void SERIAL_init(void)
@@ -40,7 +44,7 @@ void SERIAL_init(void)
     // Install UART driver (we don't need an event queue here)
     // tx buffer 0 so the tx time doesn't overlap with the job wait time
     //  by returning before the job is written
-    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
+    uart_driver_install(UART_NUM_1, BAP_BUF_SIZE * 2, BAP_BUF_SIZE * 2, 0, NULL, 0);
 }
 
 void SERIAL_set_baud(int baud)
@@ -51,13 +55,10 @@ void SERIAL_set_baud(int baud)
 
 int SERIAL_send(uint8_t *data, int len, bool debug)
 {
-    if (debug)
-    {
-        printf("tx: ");
-        prettyHex((unsigned char *)data, len);
-        printf("\n");
-    }
-
+    ESP_LOGI(TAG, "tx: ");
+    prettyHex((unsigned char *)data, len);
+    ESP_LOGI(TAG, "\n");
+   
     return uart_write_bytes(UART_NUM_1, (const char *)data, len);
 }
 
@@ -100,4 +101,63 @@ void SERIAL_debug_rx(void)
 void SERIAL_clear_buffer(void)
 {
     uart_flush(UART_NUM_1);
+}
+
+
+void SERIAL_init_BAP(void)
+{
+    ESP_LOGI(TAG, "Initializing serial");
+    // Configure UART1 parameters
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    // Configure UART1 parameters
+    uart_param_config(UART_NUM_2, &uart_config);
+    // Set UART1 pins(TX: IO17, RX: I018)
+    uart_set_pin(UART_NUM_2, UARTBAP_TXD, UARTBAP_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    // Install UART driver (we don't need an event queue here)
+    // tx buffer 0 so the tx time doesn't overlap with the job wait time
+    //  by returning before the job is written
+    uart_driver_install(UART_NUM_2, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
+}
+
+int SERIAL_send_BAP(uint8_t *data, int len, bool debug)
+{
+    ESP_LOGI("Serial BAP", "tx: ");
+    prettyHex((unsigned char *)data, len);
+    ESP_LOGI("Serial BAP", "\n");
+
+    return uart_write_bytes(UART_NUM_2, (const char *)data, len);
+}
+
+/// @brief waits for a serial response from the device
+/// @param buf buffer to read data into
+/// @param buf number of ms to wait before timing out
+/// @return number of bytes read, or -1 on error
+int16_t SERIAL_rx_BAP(uint8_t *buf, uint16_t size, uint16_t timeout_ms)
+{
+    int16_t bytes_read = uart_read_bytes(UART_NUM_2, buf, size, timeout_ms / portTICK_PERIOD_MS);
+
+    size_t buff_len = 0;
+    if (bytes_read > 0) 
+    {
+        uart_get_buffered_data_len(UART_NUM_2, &buff_len);
+        ESP_LOGI("Serial BAP", "rx: ");
+        prettyHex((unsigned char*) buf, bytes_read);
+        ESP_LOGI("Serial BAP", " [%d]\n", buff_len);
+    }
+
+    return bytes_read;
+}
+
+
+void SERIAL_clear_buffer_BAP(void)
+{
+    uart_flush(UART_NUM_2);
 }
