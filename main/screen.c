5d729ece7d037bbd405be96bdd763fe7811f37ad
mutatrum <mutatrum@gmail.com>
2025-04-04 14:38:12 +0200

    Fix Connected! display on screen

--- main/screen.c
+++ main/screen.c
@@ -7,14 +7,32 @@
 #include "global_state.h"
 #include "screen.h"
 
-// static const char * TAG = "screen";
+typedef enum {
+    SCR_SELF_TEST,
+    SCR_OVERHEAT,
+    SCR_ASIC_STATUS,
+    SCR_CONFIGURE,
+    SCR_FIRMWARE_UPDATE,
+    SCR_CONNECTION,
+    SCR_LOGO,
+    SCR_URLS,
+    SCR_STATS,
+    MAX_SCREENS,
+} screen_t;
+
+#define SCREEN_UPDATE_MS 500
+
+#define SCR_CAROUSEL_START SCR_URLS
+#define SCR_CAROUSEL_END SCR_STATS
 
 extern const lv_img_dsc_t logo;
 
 static lv_obj_t * screens[MAX_SCREENS];
+static int delays_ms[MAX_SCREENS] = {0, 0, 0, 0, 0, 1000, 5000, 10000, 10000};
 
 static screen_t current_screen = -1;
-static TickType_t current_screen_counter;
+static int current_screen_time_ms;
+static int current_screen_delay_ms;
 
 static GlobalState * GLOBAL_STATE;
 
@@ -89,10 +107,9 @@ static lv_obj_t * create_scr_overheat(SystemModule * module) {
     lv_label_set_text(label2, "Power, frequency and fan configurations have been reset. Go to AxeOS to reconfigure device.");
 
     lv_obj_t *label3 = lv_label_create(scr);
-    lv_label_set_text(label3, "Device IP:");
+    lv_label_set_text(label3, "IP Address:");
 
     ip_addr_scr_overheat_label = lv_label_create(scr);
-    lv_label_set_text(ip_addr_scr_overheat_label, module->ip_addr_str);
 
     return scr;
 }
@@ -198,13 +215,11 @@ static lv_obj_t * create_scr_urls(SystemModule * module) {
     mining_url_scr_urls_label = lv_label_create(scr);
     lv_obj_set_width(mining_url_scr_urls_label, LV_HOR_RES);
     lv_label_set_long_mode(mining_url_scr_urls_label, LV_LABEL_LONG_SCROLL_CIRCULAR);
-    lv_label_set_text(mining_url_scr_urls_label, module->is_using_fallback ? module->fallback_pool_url : module->pool_url);
 
     lv_obj_t *label3 = lv_label_create(scr);
-    lv_label_set_text(label3, "Bitaxe IP:");
+    lv_label_set_text(label3, "IP Address:");
 
     ip_addr_scr_urls_label = lv_label_create(scr);
-    lv_label_set_text(ip_addr_scr_urls_label, module->ip_addr_str);
 
     return scr;
 }
@@ -241,7 +256,8 @@ static void screen_show(screen_t screen)
         }
 
         current_screen = screen;
-        current_screen_counter = 0;
+        current_screen_time_ms = 0;
+        current_screen_delay_ms = delays_ms[screen];
     }
 }
 
@@ -305,27 +321,13 @@ static void screen_update_cb(lv_timer_t * timer)
             lv_label_set_text(wifi_status_label, module->wifi_status);
         }
         screen_show(SCR_CONNECTION);
-        return;
-    }
-
-    current_screen_counter++;
-
-    // Logo
-
-    if (current_screen < SCR_LOGO) {
-        screen_show(SCR_LOGO);
-        return;
-    }
-
-    if (current_screen == SCR_LOGO) {
-        if (LOGO_DELAY_COUNT > current_screen_counter) {
-            return;
-        }
-        screen_show(SCR_CAROUSEL_START);
+        current_screen_time_ms = 0;
         return;
     }
 
     // Carousel
+
+    current_screen_time_ms += SCREEN_UPDATE_MS;
 
     PowerManagementModule * power_management = &GLOBAL_STATE->POWER_MANAGEMENT_MODULE;
 
@@ -372,7 +374,7 @@ static void screen_update_cb(lv_timer_t * timer)
     current_difficulty = module->best_session_nonce_diff;
     current_chip_temp = power_management->chip_temp_avg;
 
-    if (CAROUSEL_DELAY_COUNT > current_screen_counter || found_block) {
+    if (current_screen_time_ms <= current_screen_delay_ms || found_block) {
         return;
     }
 
@@ -381,9 +383,7 @@ static void screen_update_cb(lv_timer_t * timer)
 
 void screen_next()
 {
-    if (current_screen >= SCR_CAROUSEL_START) {
-        screen_show(current_screen == SCR_CAROUSEL_END ? SCR_CAROUSEL_START : current_screen + 1);
-    }
+    screen_show(current_screen == SCR_CAROUSEL_END ? SCR_CAROUSEL_START : current_screen + 1);
 }
 
 esp_err_t screen_start(void * pvParameters)
