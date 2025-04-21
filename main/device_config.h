3020a96abb569add9624ff9628cd6133d5ba9645
mutatrum <mutatrum@gmail.com>
2025-04-21 15:19:58 +0200

    Refactor power.c

--- main/device_config.h
+++ main/device_config.h
@@ -43,6 +43,9 @@ typedef struct {
     const char * name;
     AsicConfig asic;
     uint8_t asic_count;
+    uint16_t max_power;
+    uint16_t power_offset;
+    uint16_t nominal_voltage;
 } FamilyConfig;
 
 typedef struct {
@@ -72,12 +75,12 @@ static const AsicConfig ASIC_BM1366 = { .model = BM1366, .name = "BM1366", .chip
 static const AsicConfig ASIC_BM1368 = { .model = BM1368, .name = "BM1368", .chip_id = 1368, .default_frequency_mhz = 490, .default_voltage_mv = 1166, .difficulty = 256, .core_count =  80, .small_core_count = 1276 };
 static const AsicConfig ASIC_BM1370 = { .model = BM1370, .name = "BM1370", .chip_id = 1370, .default_frequency_mhz = 525, .default_voltage_mv = 1150, .difficulty = 256, .core_count = 128, .small_core_count = 2040 };
 
-static const FamilyConfig FAMILY_MAX         = { .name = "Max",        .asic = ASIC_BM1397, .asic_count = 1 };
-static const FamilyConfig FAMILY_ULTRA       = { .name = "Ultra",      .asic = ASIC_BM1366, .asic_count = 1 };
-static const FamilyConfig FAMILY_HEX         = { .name = "Hex",        .asic = ASIC_BM1368, .asic_count = 6 };
-static const FamilyConfig FAMILY_SUPRA       = { .name = "Supra",      .asic = ASIC_BM1368, .asic_count = 1 };
-static const FamilyConfig FAMILY_GAMMA       = { .name = "Gamma",      .asic = ASIC_BM1370, .asic_count = 1 };
-static const FamilyConfig FAMILY_GAMMA_TURBO = { .name = "GammaTurbo", .asic = ASIC_BM1370, .asic_count = 2 };
+static const FamilyConfig FAMILY_MAX         = { .name = "Max",        .asic = ASIC_BM1397, .asic_count = 1, .max_power = 25, .power_offset = 5, .nominal_voltage = 5,  };
+static const FamilyConfig FAMILY_ULTRA       = { .name = "Ultra",      .asic = ASIC_BM1366, .asic_count = 1, .max_power = 25, .power_offset = 5, .nominal_voltage = 5,  };
+static const FamilyConfig FAMILY_HEX         = { .name = "Hex",        .asic = ASIC_BM1368, .asic_count = 6, .max_power = 0,  .power_offset = 5, .nominal_voltage = 5,  };
+static const FamilyConfig FAMILY_SUPRA       = { .name = "Supra",      .asic = ASIC_BM1368, .asic_count = 1, .max_power = 40, .power_offset = 5, .nominal_voltage = 5,  };
+static const FamilyConfig FAMILY_GAMMA       = { .name = "Gamma",      .asic = ASIC_BM1370, .asic_count = 1, .max_power = 40, .power_offset = 5, .nominal_voltage = 5,  };
+static const FamilyConfig FAMILY_GAMMA_TURBO = { .name = "GammaTurbo", .asic = ASIC_BM1370, .asic_count = 2, .max_power = 60, .power_offset = 5, .nominal_voltage = 12, };
 
 static const DeviceConfig default_configs[] = {
     { .board_version = 102, .family = FAMILY_MAX,         .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .DS4432U = true, .INA260 = true, .plug_sense = true, .asic_enable = true, .power_consumption_target = 12, },
@@ -88,8 +91,8 @@ static const DeviceConfig default_configs[] = {
     { .board_version = 205, .family = FAMILY_ULTRA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .DS4432U = true, .INA260 = true, .plug_sense = true, .asic_enable = true, .power_consumption_target = 12, },
     { .board_version = 400, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_INTERNAL,                                         .DS4432U = true, .INA260 = true, .plug_sense = true, .asic_enable = true, .power_consumption_target = 12, },
     { .board_version = 401, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_INTERNAL,                                         .DS4432U = true, .INA260 = true, .plug_sense = true, .asic_enable = true, .power_consumption_target = 12, },
-    { .board_version = 402, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .TPS546 = true,                                                           .power_consumption_target = 5, },
-    { .board_version = 403, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .TPS546 = true,                                                           .power_consumption_target = 5, },
+    { .board_version = 402, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .TPS546 = true,                                                           .power_consumption_target = 5,  },
+    { .board_version = 403, .family = FAMILY_SUPRA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL,                                         .TPS546 = true,                                                           .power_consumption_target = 5,  },
     { .board_version = 601, .family = FAMILY_GAMMA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL, .emc2101_config = EMC2101_CONFIG_GAMMA, .TPS546 = true,                                                           .power_consumption_target = 19, },
     { .board_version = 602, .family = FAMILY_GAMMA,       .display = SSD1306, .thermal = EMC2101_EXTERNAL, .emc2101_config = EMC2101_CONFIG_GAMMA, .TPS546 = true,                                                           .power_consumption_target = 22, },
     { .board_version = 800, .family = FAMILY_GAMMA_TURBO, .display = SSD1306, .thermal = EMC2103,                                                  .TPS546 = true,                                                           .power_consumption_target = 12, },
