3168886749d15fb66db3b1b74a5019e42d98d9bb
terratec <989319+terratec@users.noreply.github.com>
2025-05-14 08:50:08 +0200

    Add chart data source selection

--- main/http_server/axe-os/src/models/ISystemInfo.ts
+++ main/http_server/axe-os/src/models/ISystemInfo.ts
@@ -1,3 +1,5 @@
+import { eChartLabel } from './enum/eChartLabel';
+
 interface ISharesRejectedStat {
     message: string;
     count: number;
@@ -56,6 +58,8 @@ export interface ISystemInfo {
     temptarget: number,
     fanrpm: number,
     statsFrequency: number,
+    chartY1Data: eChartLabel,
+    chartY2Data: eChartLabel,
     coreVoltageActual: number,
 
     boardtemp1?: number,
