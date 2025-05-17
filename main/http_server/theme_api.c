0963adedcd7bd16f10c85ac3e566e3410ab6ea36
Ben C <ben@advancedcryptoservices.com>
2025-05-17 09:47:14 -0500

    correctly converting to a string

--- main/http_server/theme_api.c
+++ main/http_server/theme_api.c
@@ -3,6 +3,7 @@
 #include "esp_log.h"
 #include "nvs_config.h"
 #include "cJSON.h"
+#include <string.h>
 
 static const char *TAG = "theme_api";
 uiTheme_t currentTheme;
@@ -18,67 +19,147 @@ themePreset_t getCurrentThemePreset(void) {
 void initializeTheme(themePreset_t preset) {
     switch (preset) {
         case THEME_BITAXE_RED:
-            currentTheme.primaryColor = "#F80421";
-            currentTheme.secondaryColor = "#FC4D62";
-            currentTheme.backgroundColor = "#070D17";
-            currentTheme.textColor = "#F80421";
-            currentTheme.borderColor = "#FC4D62";
+            strncpy(currentTheme.primaryColor, "#F80421", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#FC4D62", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#070D17", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#F80421", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#FC4D62", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_BITAXE_RED;
             break;
         case THEME_BLOCKSTREAM_JADE:
-            currentTheme.primaryColor = "#00FF00";
-            currentTheme.secondaryColor = "#00FF00";
-            currentTheme.backgroundColor = "#000000";
-            currentTheme.textColor = "#00FF00";
-            currentTheme.borderColor = "#00FF00";
+            strncpy(currentTheme.primaryColor, "#00B093", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#006D62", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#111316", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#21CCAB", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#01544A", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_BLOCKSTREAM_JADE;
             break;
         case THEME_BLOCKSTREAM_BLUE:
-            currentTheme.primaryColor = "#0000FF";
-            currentTheme.secondaryColor = "#0000FF";
-            currentTheme.backgroundColor = "#000000";
-            currentTheme.textColor = "#0000FF";
-            currentTheme.borderColor = "#0000FF";
+            strncpy(currentTheme.primaryColor, "#00C3FF", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#00C3FF", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#111316", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#00C3FF", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#00C3FF", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_BLOCKSTREAM_BLUE;
             break;
         case THEME_SOLO_SATOSHI:
-            currentTheme.primaryColor = "#0000FF";
-            currentTheme.secondaryColor = "#0000FF";
-            currentTheme.backgroundColor = "#000000";
-            currentTheme.textColor = "#0000FF";
-            currentTheme.borderColor = "#0000FF";
+            strncpy(currentTheme.primaryColor, "#F80421", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#F7931A", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#070D17", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#FFFFFF", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#F7931A", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_SOLO_SATOSHI;
             break;
         case THEME_SOLO_MINING_CO:
-            currentTheme.primaryColor = "#0000FF";
-            currentTheme.secondaryColor = "#0000FF";
-            currentTheme.backgroundColor = "#000000";   
-            currentTheme.textColor = "#0000FF";
-            currentTheme.borderColor = "#0000FF";
+            strncpy(currentTheme.primaryColor, "#F15900", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#C5900F", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#111316", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#FFFFFF", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#C5900F", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_SOLO_MINING_CO;
             break;
         case THEME_BTCMAGAZINE:
-            currentTheme.primaryColor = "#0000FF";
-            currentTheme.secondaryColor = "#0000FF";
-            currentTheme.backgroundColor = "#000000";
-            currentTheme.textColor = "#0000FF";
-            currentTheme.borderColor = "#0000FF";
+            strncpy(currentTheme.primaryColor, "#FF9500", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#FF9500", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#111316", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#FFFFFF", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#FF9500", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_BTCMAGAZINE;
             break;
         case THEME_VOSKCOIN:
-            currentTheme.primaryColor = "#0000FF";  
-            currentTheme.secondaryColor = "#0000FF";
-            currentTheme.backgroundColor = "#000000";
-            currentTheme.textColor = "#0000FF";
-            currentTheme.borderColor = "#0000FF";
+            strncpy(currentTheme.primaryColor, "#23B852", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#23B852", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#111316", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#FFFFFF", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#23B852", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_VOSKCOIN;
             break;
         default:
-            currentTheme.primaryColor = "#A7F3D0";
-            currentTheme.secondaryColor = "#A7F3D0";
-            currentTheme.backgroundColor = "#161f1b";
-            currentTheme.textColor = "#A7F3D0";
-            currentTheme.borderColor = "#A7F3D0";
+            strncpy(currentTheme.primaryColor, "#A7F3D0", sizeof(currentTheme.primaryColor) - 1);
+            currentTheme.primaryColor[sizeof(currentTheme.primaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.secondaryColor, "#A7F3D0", sizeof(currentTheme.secondaryColor) - 1);
+            currentTheme.secondaryColor[sizeof(currentTheme.secondaryColor) - 1] = '\0';
+            
+            strncpy(currentTheme.backgroundColor, "#161F1B", sizeof(currentTheme.backgroundColor) - 1);
+            currentTheme.backgroundColor[sizeof(currentTheme.backgroundColor) - 1] = '\0';
+            
+            strncpy(currentTheme.textColor, "#A7F3D0", sizeof(currentTheme.textColor) - 1);
+            currentTheme.textColor[sizeof(currentTheme.textColor) - 1] = '\0';
+            
+            strncpy(currentTheme.borderColor, "#A7F3D0", sizeof(currentTheme.borderColor) - 1);
+            currentTheme.borderColor[sizeof(currentTheme.borderColor) - 1] = '\0';
+            
             currentTheme.themePreset = THEME_ACS_DEFAULT;
             break;
     }
