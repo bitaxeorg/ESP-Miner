7806e2524926cc565895b453e4fc5f71df1888ad
terratec <989319+terratec@users.noreply.github.com>
2025-06-14 15:30:13 +0200

    Use enum for data sources

--- main/http_server/axe-os/src/app/components/home/home.component.ts
+++ main/http_server/axe-os/src/app/components/home/home.component.ts
@@ -272,7 +272,9 @@ export class HomeComponent {
         idxChartY1Data = -1;
       }
 
-      if (stats.chartY2Data === eChartLabel.hashrate) {
+      if (stats.chartY1Data === stats.chartY2Data) {
+        idxChartY2Data = idxChartY1Data;
+      } else if (stats.chartY2Data === eChartLabel.hashrate) {
         idxChartY2Data = 1;
       } else if (stats.chartY2Data === eChartLabel.power) {
         idxChartY2Data = 2;
@@ -284,6 +286,24 @@ export class HomeComponent {
 
       stats.statistics.forEach(element => {
         element[idxHashrate] = element[idxHashrate] * 1000000000; // convert to H/s
+        switch (stats.chartY1Data) {
+          case eChartLabel.asicVoltage:
+          case eChartLabel.voltage:
+          case eChartLabel.current:
+            element[idxChartY1Data] = element[idxChartY1Data] / 1000;
+            break;
+          default:
+            break;
+        }
+        switch (stats.chartY2Data) {
+          case eChartLabel.asicVoltage:
+          case eChartLabel.voltage:
+          case eChartLabel.current:
+            element[idxChartY2Data] = element[idxChartY2Data] / 1000;
+            break;
+          default:
+            break;
+        }
 
         this.dataLabel.push(new Date().getTime() - stats.currentTimestamp + element[idxTimestamp]);
         this.hashrateData.push(element[idxHashrate]);
