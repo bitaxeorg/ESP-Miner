6e78323dfb946bfea37b853cc9c730d4aa157f93
killerink <killerink@email.de>
2025-07-14 13:03:50 +0200

    clean thermal

--- main/thermal/thermal.c
+++ main/thermal/thermal.c
@@ -5,6 +5,10 @@
 
 static const char * TAG = "thermal";
 
+esp_err_t (*set_fan_percent)(float percent);
+uint16_t (*get_fan_speed)();
+float (*get_chip_temp)();
+
 esp_err_t Thermal_init()
 {
     if (DEVICE_CONFIG.EMC2101) {
@@ -18,10 +22,20 @@ esp_err_t Thermal_init()
             EMC2101_set_ideality_factor(DEVICE_CONFIG.emc_ideality_factor);
             EMC2101_set_beta_compensation(DEVICE_CONFIG.emc_beta_compensation);
         }
+        set_fan_percent = EMC2101_set_fan_speed;
+        get_fan_speed = EMC2101_get_fan_speed;
+        if (DEVICE_CONFIG.emc_internal_temp) {
+            get_chip_temp = EMC2101_get_internal_temp;
+        } else {
+            get_chip_temp = EMC2101_get_external_temp;
+        }
         return res;
     }
     if (DEVICE_CONFIG.EMC2103) {
         ESP_LOGI(TAG, "Initializing EMC2103 (Temperature offset: %dC)", DEVICE_CONFIG.emc_temp_offset);
+        set_fan_percent = EMC2103_set_fan_speed;
+        get_fan_speed = EMC2103_get_fan_speed;
+        get_chip_temp = EMC2103_get_external_temp;
         return EMC2103_init();
     }
 
@@ -31,24 +45,12 @@ esp_err_t Thermal_init()
 // percent is a float between 0.0 and 1.0
 esp_err_t Thermal_set_fan_percent(float percent)
 {
-    if (DEVICE_CONFIG.EMC2101) {
-        EMC2101_set_fan_speed(percent);
-    }
-    if (DEVICE_CONFIG.EMC2103) {
-        EMC2103_set_fan_speed(percent);
-    }
-    return ESP_OK;
+    return set_fan_percent(percent);
 }
 
 uint16_t Thermal_get_fan_speed()
 {
-    if (DEVICE_CONFIG.EMC2101) {
-        return EMC2101_get_fan_speed();
-    }
-    if (DEVICE_CONFIG.EMC2103) {
-        return EMC2103_get_fan_speed();
-    }
-    return 0;
+    return get_fan_speed();
 }
 
 float Thermal_get_chip_temp()
@@ -56,17 +58,6 @@ float Thermal_get_chip_temp()
     if (!SYSTEM_MODULE.ASIC_initalized) {
         return -1;
     }
-
     int8_t temp_offset = DEVICE_CONFIG.emc_temp_offset;
-    if (DEVICE_CONFIG.EMC2101) {
-        if (DEVICE_CONFIG.emc_internal_temp) {
-            return EMC2101_get_internal_temp() + temp_offset;
-        } else {
-            return EMC2101_get_external_temp() + temp_offset;
-        }
-    }
-    if (DEVICE_CONFIG.EMC2103) {
-        return EMC2103_get_external_temp() + temp_offset;
-    }
-    return -1;
+    return get_chip_temp() +temp_offset;
 }
