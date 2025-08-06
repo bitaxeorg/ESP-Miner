5d43096d2910e947ba5cece5d0e7e4f9d6435763
Terry Chern <terry@yoolen.dev>
2025-08-06 22:56:13 +0000

    forgot constants

---
+++ main/http_server/axe-os/src/app/constants.ts
@@ -0,0 +1,3 @@
+export const TOOLTIPS = {
+  HOSTNAME: "The name this miner will use on your local network. Choose a unique name to help you identify it easily (e.g., 'miner-01' or 'garage-miner'). This is useful when managing multiple miners."
+} as const;
