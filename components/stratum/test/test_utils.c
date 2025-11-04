441867ca722f41a06cc984cfd988d00c1391a447
mutatrum <mutatrum@gmail.com>
2025-11-04 11:14:43 +0100

    Optimize construct_bm_job
    This PR eliminates several inefficient codepaths
     * Limit back and forth reversing of byte or word orders;
     * Remove merkle_root_be and prev_block_hash_be from bm_job;
     * All parameters are converted to binary once;
     * Stack allocation instead of malloc;
     * Remove unused functions;
     * Unroll byte and word reversal functions.

--- components/stratum/test/test_utils.c
+++ components/stratum/test/test_utils.c
@@ -2,10 +2,13 @@
 #include "utils.h"
 #include <string.h>
 
-TEST_CASE("Test double sha", "[utils]")
+TEST_CASE("Test double_sha256_bin", "[utils]")
 {
-    const char *input = "68656c6c6f";
-    char *output = double_sha256(input);
+    uint8_t hash[32];
+    char output[65];
+    const uint8_t *input = { 0x68, 0x65, 0x6c, 0x6c, 0x6f };
+    double_sha256_bin(input, 5, result);
+    bin2hex(hash, 32, output, 65);
     TEST_ASSERT_EQUAL_STRING("9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50", output);
 }
 
--- main/tasks/create_jobs_task.c
+++ main/tasks/create_jobs_task.c
@@ -87,21 +87,17 @@ static void generate_work(GlobalState *GLOBAL_STATE, mining_notify *notification
     //print generated extranonce_2
     //ESP_LOGI(TAG, "Generated extranonce_2: %s", extranonce_2_str);
 
-    char *coinbase_tx = construct_coinbase_tx(notification->coinbase_1, notification->coinbase_2, GLOBAL_STATE->extranonce_str, extranonce_2_str);
-    if (coinbase_tx == NULL) {
-        ESP_LOGE(TAG, "Failed to construct coinbase_tx");
-        return;
-    }
+    uint8_t coinbase_tx_hash[32];
+    calculate_coinbase_tx_hash(notification->coinbase_1, notification->coinbase_2, GLOBAL_STATE->extranonce_str, extranonce_2_str, coinbase_tx_hash);
 
-    char merkle_root[65];
-    calculate_merkle_root_hash(coinbase_tx, (uint8_t(*)[32])notification->merkle_branches, notification->n_merkle_branches, merkle_root);
+    uint8_t merkle_root[32];
+    calculate_merkle_root_hash(coinbase_tx_hash, (uint8_t(*)[32])notification->merkle_branches, notification->n_merkle_branches, merkle_root);
 
     bm_job next_job = construct_bm_job(notification, merkle_root, GLOBAL_STATE->version_mask, difficulty);
 
     bm_job *queued_next_job = malloc(sizeof(bm_job));
     if (queued_next_job == NULL) {
         ESP_LOGE(TAG, "Failed to allocate memory for queued_next_job");
-        free(coinbase_tx);
         return;
     }
 
@@ -111,6 +107,4 @@ static void generate_work(GlobalState *GLOBAL_STATE, mining_notify *notification
     queued_next_job->version_mask = GLOBAL_STATE->version_mask;
 
     queue_enqueue(&GLOBAL_STATE->ASIC_jobs_queue, queued_next_job);
-
-    free(coinbase_tx);
 }
