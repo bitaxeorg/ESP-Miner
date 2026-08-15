809ad69c2f09fa7f9de7491dd4d862ad95198e5b
Distortions81 <carlotto81@gmail.com>
2026-08-15 14:40:39 -0600

    refactor: isolate LAN validation and its tests

--- components/api_rx/api_rx.c
+++ components/api_rx/api_rx.c
@@ -7,38 +7,6 @@
 
 #include "api_rx.h"
 
-bool api_rx_websocket_payload_fits(size_t payload_len)
-{
-    return payload_len <= API_RX_MAX_WEBSOCKET_PAYLOAD_SIZE;
-}
-
-bool api_rx_websocket_origin_matches_host(const char *origin, const char *host)
-{
-    if (origin == NULL || host == NULL || host[0] == '\0') {
-        return false;
-    }
-
-    const char *authority = NULL;
-    static const char http_prefix[] = "http://";
-    static const char https_prefix[] = "https://";
-
-    if (strncasecmp(origin, http_prefix, sizeof(http_prefix) - 1) == 0) {
-        authority = origin + sizeof(http_prefix) - 1;
-    } else if (strncasecmp(origin, https_prefix, sizeof(https_prefix) - 1) == 0) {
-        authority = origin + sizeof(https_prefix) - 1;
-    } else {
-        return false;
-    }
-
-    size_t authority_len = strcspn(authority, "/?#");
-    if (authority_len == 0 || authority[authority_len] != '\0') {
-        return false;
-    }
-
-    size_t host_len = strlen(host);
-    return authority_len == host_len && strncasecmp(authority, host, host_len) == 0;
-}
-
 bool api_rx_ipv4_address_is_lan(const unsigned char address[4])
 {
     if (address == NULL) {
--- components/api_rx/test/test_api_rx.c
+++ components/api_rx/test/test_api_rx.c
@@ -1,57 +1,5 @@
-#include <stdint.h>
-
 #include "api_rx.h"
 #include "unity.h"
-
-TEST_CASE("WebSocket payload limit has strict boundary", "[api_rx]")
-{
-    TEST_ASSERT_TRUE(api_rx_websocket_payload_fits(0));
-    TEST_ASSERT_TRUE(api_rx_websocket_payload_fits(API_RX_MAX_WEBSOCKET_PAYLOAD_SIZE));
-    TEST_ASSERT_FALSE(api_rx_websocket_payload_fits(API_RX_MAX_WEBSOCKET_PAYLOAD_SIZE + 1U));
-    TEST_ASSERT_FALSE(api_rx_websocket_payload_fits(SIZE_MAX));
-}
-
-TEST_CASE("WebSocket origin must match request host", "[api_rx]")
-{
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "http://192.168.1.42", "192.168.1.42"));
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local", "bitaxe.local"));
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "https://BITAXE.local", "bitaxe.LOCAL"));
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local:8080", "bitaxe.local:8080"));
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "HTTP://BITAXE.LOCAL", "bitaxe.local"));
-    TEST_ASSERT_TRUE(api_rx_websocket_origin_matches_host(
-        "http://[fd00::1]:8080", "[fd00::1]:8080"));
-
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://evil.local", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local:8080", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "null", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "file://bitaxe.local", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local/", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local?query", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local#fragment", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local@example.com", "example.com"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://bitaxe.local:invalid", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "http://", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(
-        "bitaxe.local", "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host(NULL, "bitaxe.local"));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host("http://bitaxe.local", NULL));
-    TEST_ASSERT_FALSE(api_rx_websocket_origin_matches_host("http://bitaxe.local", ""));
-}
 
 TEST_CASE("LAN address validation handles IPv4 and IPv6", "[api_rx]")
 {
